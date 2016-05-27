36dba2df59e3008145917c3101d0611db7f4b384
skunkmb <mxq900@gmail.com>
2016-05-27 16:07:38 -0700

    Added nameReturner() function
    This function randomly generates names by combining adjectives and
    nouns. It will be used to give the AIs names.

---
+++ Assets/Scripts/Namer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Namer : MonoBehaviour {
+
+	public List<string> adjectivesList;
+	public List<string> nounsList;
+
+	// returns a random combination
+	string nameReturner(){
+		//vars
+		int randAdjNum = Random.Range (0, adjectivesList.Count);
+		int randNounNum = Random.Range (0, nounsList.Count);
+		//gets strings
+		string randAdj = adjectivesList [randAdjNum];
+		string randNoun = nounsList [randNounNum];
+		//final
+		string finalCombination = randAdj + randNoun;
+		//returns
+		return finalCombination;
+	}
+}
