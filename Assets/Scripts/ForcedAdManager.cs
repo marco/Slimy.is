782832df183e54582010367cf4f9c14db4cde67e
skunkmb <mxq900@gmail.com>
2016-05-29 13:36:43 -0700

    Added forced ads
    There is now a one-in-ten chance a skippable ad will be shown on the
    “game over” screen.

---
+++ Assets/Scripts/ForcedAdManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class ForcedAdManager : MonoBehaviour {
+
+	public string gameId;
+	public bool enableTest;
+	public int randRange;
+
+
+	void Start(){
+		//gets ad ready
+		Advertisement.Initialize (gameId, enableTest);
+
+		//gets a random number in range
+		int randomNum = Random.Range (0, randRange);
+		//checks if we should display ad
+		if (randomNum == 0) {
+			//idk
+			StartCoroutine("startAdShow");
+		}
+	}
+	
+	IEnumerator startAdShow(){
+		//while it's not ready...
+		while (!Advertisement.IsReady()) {
+			//i don't understand yields
+			yield return null;
+		}
+		//then finally
+		Advertisement.Show();
+	}
+}
